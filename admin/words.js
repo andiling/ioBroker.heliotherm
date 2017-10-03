573ff4133c446f46169feba924fb30408c44d9ed
andiling <andiling@users.noreply.github.com>
2017-10-03 16:42:45 +0200

    Add files via upload

---
+++ admin/words.js
@@ -0,0 +1,5 @@
+systemDictionary = {
+    "Heliotherm adapter settings":{"de": "Heliotherm Adapter Einstellungen", "ru": "Heliotherm Настройки драйвера"},
+    "Modem IP:":      {"de": "Modem IP:",   "ru": "Modem IP:"},
+    "Modem Port:":    {"en": "Modem Port:", "de": "Modem Port:",      "ru": "Modem порт:"},
+};
